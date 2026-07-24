f261d54e05722d627c35440d8137755aecd6e2af
FusionAtom360 <joe.yakligian@gmail.com>
2026-07-24 08:35:01 -0700

    Added versioning

--- update.py
+++ update.py
@@ -1,7 +1,12 @@
+import json
+
 class Updater:
     def __init__(self, current_version, latest_version):
-        self.current_version = current_version
-        self.latest_version = latest_version
+        with open('details.json', 'r') as f:
+            data = json.load(f)
+        self.current_version = data['version']
+        
+        self.latest_version = 
 
     def check_for_update(self):
         if self.current_version < self.latest_version:
